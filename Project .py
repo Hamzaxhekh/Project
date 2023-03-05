b03a0f3eb376cbc53759f31ff71f78350cfba6a4
Hamza Khalid <92250576+Hamzaxhekh@users.noreply.github.com>
2023-03-05 12:48:42 +0500

    Add files via upload

---
+++ Project .py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Title : Course Final Project
+
+# This project is basically contains some instructions and some task that is performed to show the basic understanding that we know. We have completed all the task given by the IBM team to perform in this notebook It consist of some markdown cells and also it contains code cells
+
+# Following are the languages that can be use in data science projects 
+# -> Python
+# -> R
+# -> Julia
+# -> SQL
+# 
+
+# Following are the libraries that can be use in Data science projects
+# -> Pandas
+# -> Numpy
+# -> Scikit Learn
+# -> Matplotlib
+# -> Sea Born
+# -> Keras
+# -> Tensor Flow
+
+# Following is the list of tools that can be use for data science
+# -> Jupyter Notebook
+# -> jupyter Lab
+# -> Google colab
+# -> IBM watson studio
+# 
+
+# Arithmetic expression are those expression which consist of basic arithmetic operators Like Addition,Subtraction,Multplication and Division
+# Example 
+# 16+13   This is an arithmetic expression
+# 15-12   This is an artihmetic expression
+
+# In[1]:
+
+
+a=13
+b=18
+sum = a+b
+mul = a*b
+div = a/b
+sub = a-b
+print(sum)
+print(mul)
+print(div)
+print(sub)
+
+
+# In[2]:
+
+
+minutes = 130
+#convertinng minutes into hours
+mtohours = 130 /60
+print(mtohours)
+
+
+# ## Objectives
+# 
+# -> To basically understand Jupyter Notebook
+# -> To understand the knowledge of cells
+# -> To understand how to use tools for datascience purposes
+# -> To have basic understanding of datascience tools
+# 
+
+# # Author
+# Hamza Khalid
+
+# In[ ]:
+
+
+
+
